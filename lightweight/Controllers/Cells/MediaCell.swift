a3c0f9be953ac6af46f7414dab90bddbed258f27
Sebastien Audeon <sebastien.peek@gmail.com>
2020-04-08 16:49:20 -0700

    feat: initial user interface implementation (#5)
    * feat: quick work to get the basics setup for the user interface
    
    * fix: annoying printing of response
    
    * feat: provide ability to set name, url and artwork on cell
    
    * feat: implements fetching of data, and displaying in UITableView
    
    * feat: some minor UI updates, provides ability to clear results through cancel button

---
+++ lightweight/Controllers/Cells/MediaCell.swift
@@ -0,0 +1,51 @@
+//
+//  MediaCell.swift
+//  lightweight
+//
+//  Created by Sebastien Audeon on 4/8/20.
+//  Copyright © 2020 Sebastien Audeon. All rights reserved.
+//
+
+import UIKit
+
+import apple_data_package
+
+class MediaCell: UITableViewCell {
+    
+    @IBOutlet weak var artworkImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var genreLabel: UILabel!
+    @IBOutlet weak var linkLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        // Configure the view for the selected state
+    }
+    
+    func setup(with media: Media) {
+        // Provide ability to grab the media information.
+        
+        self.nameLabel.text = media.name
+        self.linkLabel.text = media.url
+        
+        guard let mediaUrl = media.artwork, let url = URL(string: mediaUrl) else { return }
+        
+        // We need to load images in.
+        DispatchQueue.global().async { [weak self] in
+            if let data = try? Data(contentsOf: url) {
+                if let image = UIImage(data: data) {
+                    DispatchQueue.main.async {
+                        self?.artworkImageView.image = image
+                    }
+                }
+            }
+        }
+        
+    }
+
+}
---
+++ lightweight/Controllers/MainSearchViewController.swift
@@ -0,0 +1,95 @@
+//
+//  MainSearchViewController.swift
+//  lightweight
+//
+//  Created by Sebastien Audeon on 4/8/20.
+//  Copyright © 2020 Sebastien Audeon. All rights reserved.
+//
+
+import UIKit
+
+import apple_data_package
+
+class MainSearchViewController: UIViewController {
+    
+    @IBOutlet weak var searchView: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    
+    private var media: [MediaType: [Media]] = [:]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.tableFooterView = UIView()
+        
+    }
+
+}
+
+extension MainSearchViewController: UISearchBarDelegate {
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        // Grab the text out of the search bar
+        let query = searchBar.text
+        
+        // Pass the query off to the MediaManager.
+        MediaManager().search(with: query) { [weak self] (collection, error) in
+            
+            if let error = error {
+                // Alert the user.
+                print(error.localizedDescription)
+            }
+            
+            if let collection = collection {
+                self?.media = collection
+                DispatchQueue.main.async {
+                    self?.tableView.reloadData()
+                }
+            }
+            
+        }
+        
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        self.media.removeAll()
+        self.tableView.reloadData()
+    }
+    
+}
+
+extension MainSearchViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return media.keys.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // We need to get the values for the key for said section.
+        let map = media.keys.map { type -> MediaType in
+            return type
+        }
+        let key = map[section]
+        
+        return media[key]?.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "mediaCell") as? MediaCell else {
+            return UITableViewCell()
+        }
+        
+        let map = media.keys.map { type -> MediaType in
+            return type
+        }
+        let key = map[indexPath.section]
+        
+        guard let item = media[key]?[indexPath.row] else { return cell }
+        cell.setup(with: item)
+        
+        return cell
+        
+    }
+    
+}
