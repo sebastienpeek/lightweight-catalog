3d910de35b45ec12bf93a8c541c0bfd8fd77342d
Sebastien Audeon <sebastien.peek@gmail.com>
2020-04-08 16:44:35 -0700

    feat: provide ability to set name, url and artwork on cell

--- lightweight/Controllers/Cells/MediaCell.swift
+++ lightweight/Controllers/Cells/MediaCell.swift
@@ -11,6 +11,11 @@ import UIKit
 import apple_data_package
 
 class MediaCell: UITableViewCell {
+    
+    @IBOutlet weak var artworkImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var genreLabel: UILabel!
+    @IBOutlet weak var linkLabel: UILabel!
 
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -25,6 +30,22 @@ class MediaCell: UITableViewCell {
     
     func setup(with media: Media) {
         // Provide ability to grab the media information.
+        
+        self.nameLabel.text = media.name
+        self.linkLabel.text = media.url
+        
+        guard let mediaUrl = media.artwork, let url = URL(string: mediaUrl) else { return }
+        
+        // We need to load images in.
+        DispatchQueue.global().async { [weak self] in
+            if let data = try? Data(contentsOf: url) {
+                if let image = UIImage(data: data) {
+                    DispatchQueue.main.async {
+                        self?.artworkImageView.image = image
+                    }
+                }
+            }
+        }
         
     }
 
